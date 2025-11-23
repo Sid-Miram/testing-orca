ad8991cd94139f145abd667b055247dcfe582e7d
Sid-Miram <beingsilent7777@gmail.com>
2025-11-23 20:34:56 +0530

    fix: resolve build issue for dashboard feature

--- app/(app)/dashboard/components/ui/calendar.tsx
+++ app/(app)/dashboard/components/ui/calendar.tsx
@@ -1,9 +1,7 @@
 "use client";
-
 import * as React from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { DayPicker } from "react-day-picker";
-
 import { cn } from "./utils";
 import { buttonVariants } from "./button";
 
@@ -60,12 +58,10 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
-          <ChevronLeft className={cn("size-4", className)} {...props} />
-        ),
-        IconRight: ({ className, ...props }) => (
-          <ChevronRight className={cn("size-4", className)} {...props} />
-        ),
+        Chevron: ({ orientation, ...props }) => {
+          const Icon = orientation === "left" ? ChevronLeft : ChevronRight;
+          return <Icon className="size-4" {...props} />;
+        },
       }}
       {...props}
     />
--- app/(app)/dashboard/components/ui/drawer.tsx
+++ app/(app)/dashboard/components/ui/drawer.tsx
@@ -42,6 +42,7 @@ function DrawerOverlay({
       )}
       {...props}
     />
+
   );
 }
 
